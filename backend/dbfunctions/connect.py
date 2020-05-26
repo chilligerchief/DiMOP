62dd6f1acabfb3ac908f434a37067ddd51e9cc8e
Milena243 <milenaregensburger24@googlemail.com>
2020-05-26 17:00:30 +0200

    Stückliste Endpunkt hinzugefügt

--- backend/dbfunctions/connect.py
+++ backend/dbfunctions/connect.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 
 def connect_db():
-    db_connection_str = 'mysql+pymysql://backend:backendpw123@localhost/dimop'
+    db_connection_str = 'mysql+pymysql://milena:ALAQsM8W@132.187.102.201/dimop'
     db_connection = create_engine(db_connection_str)
 
     return db_connection
