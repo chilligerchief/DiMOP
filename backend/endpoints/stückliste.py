f0b7b4d5937881d11c00f9861c9eee98c11d3998
Milena243 <milenaregensburger24@googlemail.com>
2020-05-31 20:05:33 +0200

    Code hinzugefügt

---
+++ backend/endpoints/stückliste.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, request
+from dbfunctions.connect import *
+import json
+import pandas as pd
+
+listusers_bp = Blueprint('listusers', __name__)
+
+@listusers_bp.route('/getUsers', methods=["GET"])
+def listusers():
+    # Bekommt per POST Protokoll, Server, Port, Benutzername, Passwort, Datenbanktyp
+    # Speichert das in lokaler sqlite datenbank als dict ab
+
+    # {'IPPort': '4124', 'IPAddress': '12', 'protocol': 'fsdgfd', 'username': 'dfhg', 'password': 'dfgh'}
+    # print(request.json)
+
+    conn = connect_db()
+
+    konsid = request.args.get('konsid')
+    print(request.args.get('konsid'))
+
+    df = pd.read_sql_query("SELECT mara.mara_nr, mara.mat_desc, mast.bom_al, mast.user_id, mast.created_at, mast.updated_at, mast.cad_nr FROM mast LEFT JOIN mara ON mast.mara_id=mara.id WHERE kons_id='"+ konsid + "'", conn)
+
+
+    # Bei erfolg http status 200 zurückgeben an frontend
+    #ret = {"id_database_severs": database_server["id_database_severs"]}
+    print(df.head())
+    return df.to_json(orient='records'), 200, {'ContentType': 'application/json'}
