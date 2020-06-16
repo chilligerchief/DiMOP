e24ec537d4146779c384a8484b741ef7707ef4c3
Milena243 <milenaregensburger24@googlemail.com>
2020-06-16 09:26:43 +0200

    postKons code geändert

--- backend/endpoints/postKons.py
+++ backend/endpoints/postKons.py
@@ -5,17 +5,17 @@ import pandas as pd
 
 postKons_bp = Blueprint('postKons', __name__)
 
-@postKons_bp.route('/postKons', methods=["POST"])
+@postKons_bp.route('/postKons', methods=["POST", "GET"])
 def postKons():
     # Bekommt per POST Protokoll, Server, Port, Benutzername, Passwort, Datenbanktyp
     # Speichert das in lokaler sqlite datenbank als dict ab
 
     # {'IPPort': '4124', 'IPAddress': '12', 'protocol': 'fsdgfd', 'username': 'dfhg', 'password': 'dfgh'}
-    # print(request.json)
+    # print(request.json)'
 
     conn = connect_db()
 
-    df = pd.read_sql_query("insert into kons values ('7', 'kons_title_test', 'kons_desc_test', '1', '1', '2020-06-16', 'NULL', '0')",conn)
+    df = pd.read_sql_query("insert into kons values ('7', 'kons_title_test', 'kons_desc_test', '1', '1', '2020-06-16', '2020-06-16', '0')",conn)
 
 
     # Bei erfolg http status 200 zurückgeben an frontend
