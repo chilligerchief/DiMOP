f157f21ee35f838870af0faad8ebbf0ab93968ca
Milena243 <milenaregensburger24@googlemail.com>
2020-06-30 18:38:58 +0200

    neue Endpoints

---
+++ backend/endpoints/getMaco.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, request
+from dbfunctions.connect import *
+import json
+import pandas as pd
+
+getMaco_bp = Blueprint('getMaco', __name__)
+
+@getMaco_bp.route('/getMaco', methods=["GET"])
+def getMaco():
+    # Bekommt per POST Protokoll, Server, Port, Benutzername, Passwort, Datenbanktyp
+    # Speichert das in lokaler sqlite datenbank als dict ab
+
+    # {'IPPort': '4124', 'IPAddress': '12', 'protocol': 'fsdgfd', 'username': 'dfhg', 'password': 'dfgh'}
+    # print(request.json)
+
+    conn = connect_db()
+
+    stpoid = request.args.get('stpoid')
+    print(request.args.get('slid'))
+
+    df = pd.read_sql_query("SELECT maco.id, maco.stpo_id_comp_a, M1.mat_desc, S1.pos AS pos_comp_a, maco.stpo_id_comp_b, M2.mat_desc, S2.pos AS pos_comp_b, t_reltyp.`type`, t_reltyp.kind, maco.mass_product FROM maco LEFT JOIN t_reltyp on maco.t_reltyp_id=t_reltyp.id LEFT JOIN stpo AS S1 ON maco.stpo_id_comp_a=S1.id LEFT JOIN stpo AS S2 ON maco.stpo_id_comp_b=S2.id LEFT JOIN mara AS M1 ON S1.mara_id=M1.id lEFT JOIN mara AS M2 ON S2.mara_id=M2.id WHERE S1.id='"+ slid + "'", conn)
+
+
+    # Bei erfolg http status 200 zurückgeben an frontend
+    #ret = {"id_database_severs": database_server["id_database_severs"]}
+    print(df.head())
+    return df.to_json(orient='records'), 200, {'ContentType': 'application/json'}
--- backend/main.py
+++ backend/main.py
@@ -22,6 +22,8 @@ from endpoints.getReltyp import *
 from endpoints.getFam import *
 from endpoints.getComp import *
 from endpoints.getKons_test import *
+from endpoints.getUsers import *
+from endpoints.getMaco import *
 
 # SETUP STEP
 
@@ -48,6 +50,8 @@ app.register_blueprint(getReltyp_bp)
 app.register_blueprint(getFam_bp)
 app.register_blueprint(getComp_bp)
 app.register_blueprint(getKons_test_bp)
+app.register_blueprint(getUsers_bp)
+app.register_blueprint(getMaco_bp)
 
 
 if __name__ == '__main__':
