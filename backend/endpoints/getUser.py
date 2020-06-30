78c770bfdf54561af838c4cdaf786cdc0ae00952
Milena243 <milenaregensburger24@googlemail.com>
2020-06-30 14:25:33 +0200

    getUser geändert

--- backend/endpoints/getUser.py
+++ backend/endpoints/getUser.py
@@ -15,8 +15,10 @@ def getUser():
 
     conn = connect_db()
 
-    df = pd.read_sql_query("SELECT user.id, user.firstname, user.surname, user.e_mail, orga.orga_name, t_function.function FROM user LEFT join orga ON (orga.id=user.orga_id) LEFT join t_function ON (t_function.id=user.t_function_id)", conn)
+    email = request.args.get('email')
+    print(request.args.get('email'))
 
+    df = pd.read_sql_query("SELECT user.id, user.firstname, user.surname, user.e_mail, orga.orga_name, t_function.`function`, user.del_kz FROM user LEFT JOIN orga ON user.orga_id=orga.id LEFT JOIN t_function ON user.t_function_id=t_function.id where user.e_mail='"+ email + "'", conn)
 
     # Bei erfolg http status 200 zurückgeben an frontend
     #ret = {"id_database_severs": database_server["id_database_severs"]}
