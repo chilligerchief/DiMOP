3116fdf24f298a02cb9236b549147c582c629716
Adrian <adrian-hofmann@gmx.de>
2020-10-01 15:24:21 +0200

    Tree fixed and Rechtschreibung

--- backend/endpoints/tabletree.py
+++ backend/endpoints/tabletree.py
@@ -4,13 +4,9 @@ from sqlalchemy import text
 
 class Tabletree(Resource):
     def get(self, mast_id):
-        tree = findchildrean(mast_id)
-        my_list = []
-        for x in tree:
-            my_list.append(dict(x))
-        return my_list
-
-def findchildrean(mast_id):
-    sql = text("SELECT stpo.mast_id AS mast_id_parent, stpo.mara_id, mara.mara_nr, mara.mat_desc, stpo.pos, mast.id AS mast_id_childrean, stpo.height_erp, stpo.width_erp, stpo.depth_erp, stpo.unit_erp, stpo.volume_cad, stpo.unit_cad, stpo.weight_ui, stpo.qr_relevant FROM stpo left join mast ON stpo.mara_id=mast.mara_id LEFT JOIN mara ON mast.mara_id=mara.id WHERE stpo.mast_id=:mast_id")
+        return [dict(x) for x in findchildren(mast_id)]
+        
+def findchildren(mast_id):
+    sql = text("SELECT stpo.mast_id AS mast_id_parent, stpo.mara_id, mara.mara_nr, mara.mat_desc, stpo.pos, mast.id AS mast_id_children, stpo.height_erp, stpo.width_erp, stpo.depth_erp, stpo.unit_erp, stpo.volume_cad, stpo.unit_cad, stpo.weight_ui, stpo.qr_relevant FROM stpo left join mast ON stpo.mara_id=mast.mara_id LEFT JOIN mara ON mast.mara_id=mara.id WHERE stpo.mast_id=:mast_id")
     result = db.session.execute (sql, params={"mast_id": mast_id})
     return result.fetchall()
