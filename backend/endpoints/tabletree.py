fdca8c18b0a5de953f08a00e8f65c780f067da2e
TobiPrae <50541402+TobiPrae@users.noreply.github.com>
2020-10-20 15:30:36 +0200

    Update tabletree.py

--- backend/endpoints/tabletree.py
+++ backend/endpoints/tabletree.py
@@ -15,7 +15,7 @@ class Tabletree(Resource):
         bom = pd.read_sql_table('bom', db)
 
         result_list = []
-        result = [1, 0]
+        result = [1, None]
 
         result_df = mat.loc[mat["id"] == mat_id]
 
@@ -26,7 +26,9 @@ class Tabletree(Resource):
 
         result_list = getChildren(mat_id, result_list, mat, bom)
 
-        result_df = pd.DataFrame(result_list)
+        result_df = pd.DataFrame(result_list).fillna(
+            np.nan).replace([np.nan], [None])
+
         result_df = result_df.rename(
             columns={0: "result_id", 1: "parent_id", 2: "mat_id", 3: "mat_desc", 4: "mat_id_int", 5: "mat_desc_int", 6: "cad_id", 7: "mara_plast_id", 8: "mat_rw", 9: "height", 10: "width", 11: "depth", 12: "unit", 13: "weight", 14: "weight_unit", 15: "volume", 16: "volume_unit", 17: "is_atomic", 18: "orga_id"})
 
