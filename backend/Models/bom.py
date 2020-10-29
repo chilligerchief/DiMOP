d0f63a5064835a441e0eda0b37de55592f7c10aa
TobiPrae <50541402+TobiPrae@users.noreply.github.com>
2020-10-29 10:53:43 +0100

    bom endpoint

---
+++ backend/Models/bom.py
@@ -0,0 +1,40 @@
+from dbfunctions.connect import db
+from datetime import datetime
+from sqlalchemy import text
+
+
+class BomModel(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    mat_id = db.Column(db.Integer)
+    parent_mat_id = db.Column(db.Integer)
+
+    def __init__(self, mat_id, parent_mat_id):
+        self.mat_id = mat_id
+        self.parent_mat_id = parent_mat_id
+
+
+def json(self):
+    return {"id": self.id, "mat_id": self.mat_id, "parent_mat_id": self.parent_mat_id}
+
+
+@classmethod
+def find_by_id(cls, _id):
+    return cls.query.filter_by(id=_d)
+
+
+@classmethod
+def fin_all(cls):
+    sql = text("SELECT * FROM bom")
+    result = db.session.execute(sql)
+    return result.fetchall()
+
+
+def save_to_db(self):
+    db.session.add(self)
+    db.session.commit()
+
+
+def delete_from_db(self):
+    db.session.delete(self)
+    db.session.commit()
---
+++ backend/endpoints/bom.py
@@ -0,0 +1,39 @@
+from flask_restful import Resource, reqparse
+from Models.bom import BomModel
+
+
+class BomPost(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument("mat_id",
+                        type=int,
+                        required=True,
+                        help="This field cannot be blank.")
+    parser.add_argument("parent_mat_id",
+                        type=int,
+                        required=True,
+                        help="This field cannot be blank.")
+
+    def post(self):
+        data = BomPost.parser.parse_args()
+
+        bom = BomPost(**data)
+        bom.save_to_db()
+
+        return {"bom": "entry created successfully."}, 201
+
+
+class Bom(Resource):
+    def delete(self, _id):
+        bom = BomModel.find_by_id(_id).first()
+        if bom:
+            bom: delete_from_db()
+        return {"bom": "material deleted successfully"}
+
+
+class BomGet(Resource):
+    def get(self):
+        bom = BomModel.find_all()
+        my_list = []
+        for x in bom:
+            my_list.append(dict(x))
+        return my_list
