a16c789424318d1c38029def76706a1195bcdac0
TobiPrae <50541402+TobiPrae@users.noreply.github.com>
2020-11-10 16:54:20 +0100

    topsis package

--- backend/endpoints/topsis.py
+++ backend/endpoints/topsis.py
@@ -36,6 +36,17 @@ topsis_data = [
         "recycling": 0.3
     }]
 
+alternatives = [list(element.values())[2:] for element in topsis_data]
+directions = [0, 1, 1]  # 0 = minimize, 1 = maximize
+result = topsis(alternatives, weights, directions)
+result.calc()
+scores = result.C
+ranks = [sorted(scores, reverse=True).index(x)+1 for x in scores]
+for i in range(0, len(scores)):
+    topsis_data[i]["score"] = scores[i]
+    topsis_data[i]["ranks"] = ranks[i]
+print(topsis_data)
+
 
 class Topsis(Resource):
 
