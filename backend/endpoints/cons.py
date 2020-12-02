1141afb1026f7a98b15e01a5dd9f39b0b1f15bf2
TobiPrae <50541402+TobiPrae@users.noreply.github.com>
2020-12-02 11:15:48 +0100

    cons

--- backend/endpoints/cons.py
+++ backend/endpoints/cons.py
@@ -7,7 +7,7 @@ from flask_restful import Resource, reqparse
 from Models.cons import ConsModel
 
 
-class ConsPost(Resource):
+class Cons(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument('cons_title',
                         type=str,
@@ -30,48 +30,23 @@ class ConsPost(Resource):
                         help="This field cannot be blank."
                         )
 
-    def post(self):
-        data = ConsPost.parser.parse_args()
-
-        cons = ConsModel(**data)
-        cons.save_to_db()
-
-        return {"message": "Cons created successfully."}, 201
-
-
-class Cons(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('cons_title',
-                        type=str,
-                        required=True,
-                        help="This field cannot be blank."
-                        )
-    parser.add_argument('cons_desc',
-                        type=str,
-                        required=True,
-                        help="This field cannot be blank."
-                        )
-
-    def put(self, _id):
-        data = Cons.parser.parse_args()
-        cons = ConsModel.find_by_id(_id).first()
-        if cons:
-            cons.cons_title = data['cons_title']
-            cons.cons_desc = data['cons_desc']
-            cons.save_to_db()
-        return {'cons': 'Cons updated successfully'}
-
-    def delete(self, _id):
-        cons = ConsModel.find_by_id(_id).first()
-        if cons:
-            cons.delete_from_db()
-        return {'cons': 'Cons deleted successfully'}
-
-
-class ConsGet(Resource):
     def get(self, orga_id):
         cons = ConsModel.find_by_orga_id(orga_id)
         my_list = []
         for x in cons:
             my_list.append(dict(x))
         return my_list
+
+    def post(self):
+        data = Cons.parser.parse_args()
+
+        cons = ConsModel(**data)
+        cons.save_to_db()
+
+        return {"message": "Cons created successfully."}, 201
+
+    def delete(self, _id):
+        cons = ConsModel.find_by_id(_id).first()
+        if cons:
+            cons.delete_from_db()
+        return {'cons': 'Cons deleted successfully'}
