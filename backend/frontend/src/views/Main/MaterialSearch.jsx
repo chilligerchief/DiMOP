e81532ae0bfb330ffbcea50bf2e8438789f1442c
TobiPrae <50541402+TobiPrae@users.noreply.github.com>
2021-02-20 11:22:11 +0100

    Update MaterialSearch.jsx

--- backend/frontend/src/views/Main/MaterialSearch.jsx
+++ backend/frontend/src/views/Main/MaterialSearch.jsx
@@ -1,9 +1,56 @@
-import React, { useContext, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
+import Autocomplete from "@material-ui/core/TextField";
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: "25ch",
+    },
+  },
+  buttons: {
+    borderColor: "#005000",
+    color: "#005000",
+    textTransform: "none",
+    margin: 20,
+    height: 60,
+    width: 120,
+  },
+}));
 
 const MaterialSearch = () => {
+  const classes = useStyles();
+
+
+  const initial_filter = {
+    mat_desc: "",
+    campus_fam: "",
+    producer: "",
+    verarbeitungsmethode: "",
+  };
+  
+  const [filter, setFilter] = useState(initial_filter);
+
+
+  const materials = [
+    "Eins", 
+    "Zwei",
+    "Drei"
+  ]
 
   return (
     <div>
+      <form className={classes.root} noValidate autoComplete="off">
+      <Autocomplete
+          id="mat_desc"
+          label="Materialbeschreibung"
+          options={materials}
+          value={initial_filter.mat_desc}
+          margin="normal"
+          renderInput={(params) => <TextField {...params} label="Combo box" variant="outlined" />}
+        />
+      </form>
       <p>
         Swaggger.
       </p>
