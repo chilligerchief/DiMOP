740d0de1067b1a372d307d8a47c32fcb7fb76954
TobiPrae <50541402+TobiPrae@users.noreply.github.com>
2021-02-24 11:37:49 +0100

    Update MaterialSearch.jsx

--- backend/frontend/src/views/Main/MaterialSearch.jsx
+++ backend/frontend/src/views/Main/MaterialSearch.jsx
@@ -392,7 +392,264 @@ const MaterialSearch = () => {
           />
         </Grid>
         <Grid item xs={1}></Grid>
-        <Grid item xs={8}></Grid>
+        <Grid item xs={8}>
+          <Grid container item xs={12}>
+            <Grid item xs={5}>
+              <Autocomplete
+                id="zugmodul"
+                options={autocompleteData.zugmodul}
+                getOptionLabel={(option) => option}
+                onChange={(event) => {
+                  setZugmodul(event.target.textContent);
+                }}
+                value={zugmodul}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    className={classes.textfield}
+                    label="Zugmodul"
+                    margin="normal"
+                  />
+                )}
+              />
+            </Grid>
+            <Grid item xs={1}></Grid>
+            <Grid item xs={6}>
+              <Typography id="range-slider" gutterBottom>
+                Zugmodul
+              </Typography>
+              <Slider
+                value={zugmodulSliderValue}
+                onChange={(event, newValue) => {
+                  setZugmodulSliderValue(newValue);
+                }}
+                min={zugmodulSliderRange[0]}
+                max={zugmodulSliderRange[1]}
+                step={10}
+                valueLabelDisplay="auto"
+              ></Slider>
+            </Grid>
+          </Grid>
+          <Grid container item xs={12}>
+            <Grid item xs={5}>
+              <Autocomplete
+                id="bruchspannung"
+                options={autocompleteData.bruchspannung}
+                getOptionLabel={(option) => option}
+                onChange={(event) => setBruchspannung(event.target.textContent)}
+                value={bruchspannung}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    className={classes.textfield}
+                    label="Bruchspannung"
+                    margin="normal"
+                  />
+                )}
+              />
+            </Grid>
+            <Grid item xs={1}></Grid>
+            <Grid item xs={6}>
+              <Typography id="range-slider" gutterBottom>
+                Bruchspannung
+              </Typography>
+              <Slider
+                value={bruchspannungSliderValue}
+                onChange={(event, newValue) => {
+                  setBruchspannungSliderValue(newValue);
+                }}
+                min={bruchspannungSliderRange[0]}
+                max={bruchspannungSliderRange[1]}
+                step={1}
+                valueLabelDisplay="auto"
+              ></Slider>
+            </Grid>
+          </Grid>
+          <Grid container item xs={12}>
+            <Grid item xs={5}>
+              <Autocomplete
+                id="bruchdehnung"
+                options={autocompleteData.bruchdehnung}
+                getOptionLabel={(option) => option}
+                onChange={(event) => setBruchdehnung(event.target.textContent)}
+                value={bruchdehnung}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    className={classes.textfield}
+                    label="Bruchdehnung"
+                    margin="normal"
+                  />
+                )}
+              />
+            </Grid>
+            <Grid item xs={1}></Grid>
+            <Grid item xs={6}>
+              <Typography id="range-slider" gutterBottom>
+                Bruchdehnung
+              </Typography>
+              <Slider
+                value={bruchdehnungSliderValue}
+                onChange={(event, newValue) => {
+                  setBruchdehnungSliderValue(newValue);
+                }}
+                min={bruchdehnungSliderRange[0]}
+                max={bruchdehnungSliderRange[1]}
+                step={10}
+                valueLabelDisplay="auto"
+              ></Slider>
+            </Grid>
+          </Grid>
+
+          <Grid container item xs={12}>
+            <Grid item xs={5}>
+              {" "}
+              <Autocomplete
+                id="mvr"
+                options={autocompleteData.mvr}
+                getOptionLabel={(option) => option}
+                onChange={(event) => setMvr(event.target.textContent)}
+                value={mvr}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    className={classes.textfield}
+                    label="MVR"
+                    margin="normal"
+                  />
+                )}
+              />
+            </Grid>
+            <Grid item xs={1}></Grid>
+            <Grid item xs={6}>
+              {" "}
+              <Typography id="range-slider" gutterBottom>
+                MVR
+              </Typography>
+              <Slider
+                value={mvrSliderValue}
+                onChange={(event, newValue) => {
+                  setMvrSliderValue(newValue);
+                }}
+                min={mvrSliderRange[0]}
+                max={mvrSliderRange[1]}
+                step={1}
+                valueLabelDisplay="auto"
+              ></Slider>
+            </Grid>
+          </Grid>
+
+          <Grid container item xs={12}>
+            <Grid item xs={5}>
+              <Autocomplete
+                id="dichte"
+                options={autocompleteData.dichte}
+                getOptionLabel={(option) => option}
+                onChange={(event) => setDichte(event.target.textContent)}
+                value={dichte}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    className={classes.textfield}
+                    label="Dichte"
+                    margin="normal"
+                  />
+                )}
+              />
+            </Grid>
+            <Grid item xs={1}></Grid>
+            <Grid item xs={6}>
+              <Typography id="range-slider" gutterBottom>
+                Dichte
+              </Typography>
+              <Slider
+                value={dichteSliderValue}
+                onChange={(event, newValue) => {
+                  setDichteSliderValue(newValue);
+                }}
+                min={dichteSliderRange[0]}
+                max={dichteSliderRange[1]}
+                step={1}
+                valueLabelDisplay="auto"
+              ></Slider>
+            </Grid>
+          </Grid>
+
+          <Grid container item xs={12}>
+            <Grid item xs={5}>
+              {" "}
+              <Autocomplete
+                id="belastung"
+                options={autocompleteData.belastung}
+                getOptionLabel={(option) => option}
+                onChange={(event) => setBelastung(event.target.textContent)}
+                value={belastung}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    className={classes.textfield}
+                    label="Belastung"
+                    margin="normal"
+                  />
+                )}
+              />
+            </Grid>
+            <Grid item xs={1}></Grid>
+            <Grid item xs={6}>
+              {" "}
+              <Typography id="range-slider" gutterBottom>
+                Belastung
+              </Typography>
+              <Slider
+                value={belastungSliderValue}
+                onChange={(event, newValue) => {
+                  setBelastungSliderValue(newValue);
+                }}
+                min={belastungSliderRange[0]}
+                max={belastungSliderRange[1]}
+                step={1}
+                valueLabelDisplay="auto"
+              ></Slider>
+            </Grid>
+          </Grid>
+
+          <Grid container item xs={12}>
+            <Grid item xs={5}>
+              <Autocomplete
+                id="temperatur"
+                options={autocompleteData.temperatur}
+                getOptionLabel={(option) => option}
+                onChange={(event) => setTemperatur(event.target.textContent)}
+                value={temperatur}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    className={classes.textfield}
+                    label="Temperatur"
+                    margin="normal"
+                  />
+                )}
+              />
+            </Grid>
+            <Grid item xs={1}></Grid>
+            <Grid item xs={6}>
+              {" "}
+              <Typography id="range-slider" gutterBottom>
+                Temperatur
+              </Typography>
+              <Slider
+                value={temperaturSliderValue}
+                onChange={(event, newValue) => {
+                  setTemperaturSliderValue(newValue);
+                }}
+                min={temperaturSliderRange[0]}
+                max={temperaturSliderRange[1]}
+                step={1}
+                valueLabelDisplay="auto"
+              ></Slider>
+            </Grid>
+          </Grid>
+        </Grid>
       </Grid>
       <Grid container item xs={12}>
         <Grid
@@ -433,36 +690,32 @@ const MaterialSearch = () => {
         </Grid>
       </Grid>
       <Grid container item xs={12}>
-        <Grid container item xs={12}>
-          {resultsReturned == false ? (
-            <div></div>
-          ) : (
-            <div style={{ color: "black", marginTop: 25 }}>
-              {" "}
-              Es wurden {resultCount} Ergebnisse mit Ihren Spezifikationen
-              gefunden.
-            </div>
-          )}
-        </Grid>
+        {resultsReturned == false ? (
+          <div></div>
+        ) : (
+          <div style={{ color: "black", marginTop: 25 }}>
+            {" "}
+            Es wurden {resultCount} Ergebnisse mit Ihren Spezifikationen
+            gefunden.
+          </div>
+        )}
       </Grid>
       <Grid container item xs={12}>
-        <Grid container item xs={12}>
-          <GridDevExpress rows={resultData} columns={resultColumns}>
-            <SelectionState
-              selection={selection}
-              onSelectionChange={setSelection}
-            />
-            <FilteringState defaultFilters={[]} />
-            <IntegratedFiltering />
-            <Table />
-            <TableSelection />
-            <TableColumnVisibility />
-            <Toolbar />
-            <ColumnChooser />
-            <TableFilterRow />
-            <TableHeaderRow />
-          </GridDevExpress>
-        </Grid>
+        <GridDevExpress rows={resultData} columns={resultColumns}>
+          <SelectionState
+            selection={selection}
+            onSelectionChange={setSelection}
+          />
+          <FilteringState defaultFilters={[]} />
+          <IntegratedFiltering />
+          <Table />
+          <TableSelection />
+          <TableColumnVisibility />
+          <Toolbar />
+          <ColumnChooser />
+          <TableFilterRow />
+          <TableHeaderRow />
+        </GridDevExpress>
       </Grid>
     </div>
   );
